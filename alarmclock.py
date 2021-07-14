516b26778bf5b19d2cf471be1495116975c77697
Fue Moua <MadisonLifeAgent@gmail.com>
2021-07-14 13:32:07 -0500

    added Alarm_clock class, set methods for current time, toggle, and set alarm time. also created methods to use objects to use alarm clock/methods

--- alarmclock.py
+++ alarmclock.py
@@ -1,1 +1,47 @@
+#region - alarm clock class
+
 #alarm clock class
+class Alarm_clock:
+    # alarm clock properties
+    def __init__(self):
+        self.current_time = ""
+        self.alarm_toggle = ""
+        self.alarm_time = ""
+
+    # set or change current time of alarm clock
+    def what_time_is_it(self):
+        # prompt user for current time
+        self.current_time = input("What time is it?  ")
+
+        # display the current time user entered
+        print("The time is:  " + self.current_time)
+
+    # toggle the alarm to on or off
+    def alarm_toggle_switch(self):
+        # prompt user to toggle the alarm
+        self.alarm_toggle = input("Enter 1 to turn alarm ON, or 2 to turn alarm OFF.  ")
+
+        print(self.alarm_toggle)
+
+        # display whether alarm is on or off
+        if (self.alarm_toggle == '1'):
+            toggle_status = "Your alarm is turned ON"
+            print(toggle_status)
+        elif (self.alarm_toggle == '2'):
+            toggle_status = "Your alarm is turned OFF"
+            print(toggle_status)
+
+        else:
+            toggle_status = "Not a valid response. Try again."
+            print(toggle_status)
+
+        return toggle_status
+
+    # set alarm time
+    def set_wake_up_time(self):
+        # prompt user to set alarm time
+        self.alarm_time = input("Please set time for alarm:  ")
+
+        print("Your alarm is set to:  " + self.alarm_time)
+
+#endregion
---
+++ main.py
@@ -0,0 +1,29 @@
+#region - #import Alarm_clock class for use
+
+from alarmclock import Alarm_clock
+#endregion
+
+#region - # set the current time and display it
+
+# call the alarm clock and set the current time
+set_time = Alarm_clock()
+set_time.what_time_is_it()
+
+#endregion
+
+#region - #toggle the alarm on or off and display toggle status
+
+# call the alarm clock and toggle the alarm on or off
+toggle_alarm = Alarm_clock()
+toggle_status = toggle_alarm.alarm_toggle_switch()
+
+#endregion
+
+#region - #set the alarm time and display it
+
+set_alarm_time = Alarm_clock()
+set_alarm_time.set_wake_up_time()
+# print alarm toggle status
+print(toggle_status)
+
+#endregion
